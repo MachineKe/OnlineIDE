bc3a8cae0ced9d3a890e6665f6f74c493a162e3c
machineke <markarapsoi72@gmail.com>
2024-02-23 22:13:20 +0300

    fixing hosting issue

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get("/logout", (req, res) => {
 
 
 // Define Port for Application
-const port =  process.env.PORT || 3000;
+const port =  process.env.PORT;
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`)
 });
